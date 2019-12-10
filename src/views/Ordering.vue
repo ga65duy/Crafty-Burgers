e941fad7f228addb2fcbc423dbfaeab73fe27a9e
edorrough <evdo7617@colorado.edu>
2019-12-10 05:38:28 +0100

    Rudimentary step-by-step functionality

--- src/views/Ordering.vue
+++ src/views/Ordering.vue
@@ -3,33 +3,43 @@
     <img class="example-panel" src="@/assets/exampleImage.jpg">
     <button v-on:click="switchLang()">{{ uiLabels.language }}</button>
 
-    <h1>{{ uiLabels.ingredients }}</h1>
+    <NavButtons
+            ref="navigation"
+    >
+    </NavButtons><!--perhaps it doesn't prevent refreshes of page for some reason, it sometimes 'highlights' step 1-->
 
-    <Ingredient
-      ref="ingredient"
-      v-for="item in ingredients"
-      v-on:increment="addToOrder(item)"  
-      :item="item" 
-      :lang="lang"
-      :key="item.ingredient_id">
-    </Ingredient>
-
-    <h1>{{ uiLabels.order }}</h1>
-    {{ chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}, {{ price }} kr
-    <button v-on:click="placeOrder()">{{ uiLabels.placeOrder }}</button>
-
-    <h1>{{ uiLabels.ordersInQueue }}</h1>
-    <div>
-      <OrderItem 
-        v-for="(order, key) in orders"
-        v-if="order.status !== 'done'"
-        :order-id="key"
-        :order="order" 
-        :ui-labels="uiLabels"
+    <section><!--only want to display on steps 1 thru 6, implement with a computed field-->
+      <h1>{{ uiLabels.ingredients }}</h1>
+      <Ingredient
+        ref="ingredient"
+        v-for="item in ingredients"
+        v-on:increment="addToOrder(item)"
+        :item="item"
         :lang="lang"
-        :key="key">
-      </OrderItem>
-    </div>
+        :key="item.ingredient_id">
+      </Ingredient>
+    </section>
+
+    <section><!--only want to display on step 7-->
+      <h1>{{ uiLabels.order }}</h1>
+      {{ chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}, {{ price }} kr
+      <button v-on:click="placeOrder()">{{ uiLabels.placeOrder }}</button>
+    </section>
+
+    <section><!--only want to display on step 8-->
+      <h1>{{ uiLabels.ordersInQueue }}</h1>
+      <div>
+        <OrderItem
+          v-for="(order, key) in orders"
+          v-if="order.status !== 'done'"
+          :order-id="key"
+          :order="order"
+          :ui-labels="uiLabels"
+          :lang="lang"
+          :key="key">
+        </OrderItem>
+      </div>
+    </section>
   </div>
 </template>
 <script>
@@ -39,6 +49,7 @@
 //components
 import Ingredient from '@/components/Ingredient.vue'
 import OrderItem from '@/components/OrderItem.vue'
+import NavButtons from '@/components/NavButtons'
 
 //import methods and data that are shared between ordering and kitchen views
 import sharedVueStuff from '@/components/sharedVueStuff.js'
@@ -48,6 +59,7 @@ necessary Vue instance (found in main.js) to import your data and methods */
 export default {
   name: 'Ordering',
   components: {
+    NavButtons,
     Ingredient,
     OrderItem
   },

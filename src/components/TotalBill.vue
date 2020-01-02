ff782e67152fc8dac3b25779bfa82186fb61eae8
LauraBock <laurabock@live.de>
2020-01-02 17:15:23 +0100

    Css

--- src/components/TotalBill.vue
+++ src/components/TotalBill.vue
@@ -2,34 +2,34 @@
     <div id="priceTable">
         <table>
             <tr>
-                <th>Item</th>
-                <th>Price</th>
-                <th>Amount</th>
-                <th>Sum</th>
+                <th>{{uiLabels.item}}</th>
+                <th>{{uiLabels.price}}</th>
+                <th>{{uiLabels.amount}}</th>
+                <th>{{uiLabels.sum}}</th>
             </tr>
             <tr v-if="order.currentBurger.price > 0">
                 <td>{{name(order.currentBurger.id)}}</td>
-                <td>{{order.currentBurger.price}}</td>
+                <td>{{order.currentBurger.price}} kr</td>
                 <td>x{{order.currentBurger.amount}}</td>
-                <td>{{order.currentBurger.price * order.currentBurger.amount}}</td>
+                <td>{{order.currentBurger.price * order.currentBurger.amount}} kr</td>
             </tr>
             <tr v-for="burger in order.otherBurgers" :key="burger.id">
                 <td>{{name(burger.id)}}</td>
-                <td>{{burger.price}}</td>
+                <td>{{burger.price}} kr</td>
                 <td>x{{burger.amount}}</td>
-                <td>{{burger.price * burger.amount}}</td>
+                <td>{{burger.price * burger.amount}} kr</td>
             </tr>
             <tr v-for="(count, key) in this.order.sides" :key="key">
                 <td>{{getItemForKey(key)["ingredient_"+lang]}}</td>
-                <td>{{getItemForKey(key).selling_price}}</td>
+                <td>{{getItemForKey(key).selling_price}} kr</td>
                 <td>x{{count}}</td>
-                <td>{{getItemForKey(key).selling_price * count}}</td>
+                <td>{{getItemForKey(key).selling_price * count}} kr</td>
             </tr>
-            <tr>
+            <tr id="total">
                 <td>Total</td>
                 <td></td>
                 <td></td>
-                <td>{{getTotal()}}</td>
+                <td>{{getTotal()}} kr</td>
             </tr>
         </table>
 
@@ -70,9 +70,30 @@ export default {
 
 <style scoped>
 #priceTable {
-    font-size: 2em;
+    font-size: 1.5em;
     display: block;
     text-align: center;
-    font-family:arial;
+    font-family: comfortaa, sans-serif;
+}
+table {
+    border-collapse: collapse;
+    text-align: center;
+    width: 200%;
+}
+th {
+    padding: 10px;
+     border-bottom: 2px solid grey;
+}
+td {
+    padding: 10px;
+    border-bottom: 1px solid lightgrey;
+}
+#total td {
+    padding: 10px;
+    padding-top: 20px;
+    border-top: 3px solid grey;
+    border-bottom: none;
+    font-size: 1.5em;
+    font-weight: bold;
 }
 </style>

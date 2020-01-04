a5c5fd7833440c3d6a965bfd0e1774bdc115546e
LauraBock <laurabock@live.de>
2020-01-04 12:23:46 +0100

    buttons css

--- src/components/NavButtons.vue
+++ src/components/NavButtons.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <button v-on:click="selectedButton">{{step.label}}</button>
+    <div id="buttons">
+        <button v-bind:id="step.label" 
+         v-on:click="selectedButton">{{step.label}}</button>
     </div>
 </template>
 
@@ -25,18 +26,26 @@
 <style scoped>
 button {
     margin: 0.1em;
-    border: 2px solid grey;
-    border-radius: 20px;
-    width: auto;
-    font-size: 2em;
+    margin-right: 5px;
+    width: 130px;
+    height: 50px;
+    padding: 5px;
     text-align: center;
+    vertical-align: middle;
+    border: 1px solid grey;
+    border-radius: 20px;
     background-color: #e7e7e7;
     font-family: Comfortaa, sans-serif;
+    font-size: 1.5em;
+    color: darkslategrey;
 }
 button:focus {
     background-color: #70db70;
-    box-shadow: 0 0 16px 0 #52cc10ff;
-    border: 2px solid #70db70;
+    box-shadow: 0 0 8px 0 #52cc10ff;
+    border: 1px solid #5cd65c;
     outline: none;
+}
+#Preferences {
+    width: 190px;
 }
 </style>
